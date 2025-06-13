0dfda5656118fee61cf60146c1e83956b0fec5f3
Tsaqif Assajeed <tsaqifassajeed@gmail.com>
2025-06-13 12:39:18 +0700

    Initial Commit

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -347,7 +347,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2019</span>
+            <span>Copyright &copy; Sapras By Jariadi 2025</span>
           </div>
         </div>
       </footer>
